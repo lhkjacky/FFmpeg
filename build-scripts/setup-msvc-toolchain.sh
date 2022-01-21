bcb97c20fe2b6f6c7cccc3494235080cb135ed95
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-01-21 13:47:20 -0600

    Update scripts to handle some edge cases

--- build-scripts/setup-msvc-toolchain.sh
+++ build-scripts/setup-msvc-toolchain.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ -z "$CUDA_PATH" ]]; then
+	echo "CUDA_PATH is not set. Aborting."
+	return 1
+elif [[ -z "$VCINSTALLDIR" ]]; then
+	return "Couldn't find Visual Studio install location. Aborting."
+	exit 1
+elif [[ -z "$WindowsSdkVerBinPath" ]]; then
+	echo "WindowsSdkVerBinPath is not set. Aborting."
+	return 1
+fi
+
 export PATH="${VCINSTALLDIR}/Tools/MSVC/${VCToolsVersion}/bin/Hostx64/x64/":$PATH
 export PATH="${CUDA_PATH}/bin/":$PATH
 export PATH="${WindowsSdkVerBinPath}/x64/":$PATH
