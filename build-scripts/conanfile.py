f2aaf12d669371f8a2e06ec2f114dc37a40228fa
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-03-08 17:15:37 -0600

    Status commit: mac build script

---
+++ build-scripts/conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, tools
+
+# NOTE: when updating, place libraries in alphabetical order to reduce diffs
+
+class conanRecipe(ConanFile):
+    name = "FFmpeg"
+    #version
+    settings = ("os", "build_type", "arch")
+
+    def configure(self):
+        pass
+
+
+    def requirements(self):
+        self.requires("veai/TODO: version here")
+
+    def imports(self):
+        if self.settings.os == "Windows":
+            self.copy("*", "lib3rdparty", folder=True)
+            self.copy("*", "bin", "bin")
+            self.copy("*", "bin", "binr")
+            # self.copy('*', src='@bindirs', dst='binr')
+        if self.settings.os == "Macos":
+            self.copy("*", "include", "include")
+            self.copy("*", "lib", "lib")
---
+++ build-scripts/mac/build_mac.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OLD_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+
+if [[ "$USE_DEFAULT_PKG_CONFIG" -ne 1 ]] && [ -z "$OPENH264_X86_PKG_CONFIG_PATH" -o -z "$OPENH264_ARM_PKG_CONFIG_PATH" ]; then
+	echo 'Please ensure $OPENH264_X86_PKG_CONFIG_PATH and $OPENH264_ARM_PKG_CONFIG_PATH are set, or run this script with USE_DEFAULT_PKG_CONFIG=1'
+	exit 1
+fi	
+
+if [ -z "$2" -o -z "$3" -o \( "$1" = "1" -a \( -z "$4" -o -z "$5" \) \) ]; then
+	echo "Usage: ./build_mac.sh [veai enabled? (0 or 1)] [arm output path] [x86 output path] [conan arm] [conan x86] (extra ldflags)"
+	exit 1
+fi;
+
+CONAN_ARM=$4
+CONAN_X86=$5
+
+FLAGS="--enable-libvpx --enable-libopenh264"
+if [[ "$1" -eq 1 ]]; then
+	FLAGS="--extra-cflags=\"-I${CONAN_ARM}/include/veai $6\" --extra-ldflags=\"-L${CONAN_ARM}/lib $7\" --enable-veai $FLAGS"
+	XFLAGS="--arch=x86_64 --extra-cflags=\"-arch x86_64 -I${CONAN_ARM}/include/veai $2 \" --extra-ldflags=\"-arch x86_64 -L${CONAN_ARM}/lib $3\""
+fi
+
+set -e
+export PKG_CONFIG_PATH=$OPENH264_ARM_PKG_CONFIG_PATH:$OLD_PKG_CONFIG_PATH
+echo ./configure --prefix=$2 $FLAGS
+./configure --prefix=$2 $FLAGS
+make -j8 install
+
+export PKG_CONFIG_PATH=$OPENH264_X86_PKG_CONFIG_PATH:$OLD_PKG_CONFIG_PATH
+echo ./configure --prefix=$3 $XFLAGS
+./configure --prefix=$3 $XFLAGS
+make -j8 install
+
+export PKG_CONFIG_PATH=$OLD_PKG_CONFIG_PATH
