415213ac255e5de022445773409b2cce1b3711be
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-04-08 13:42:38 -0500

    Fix mac dependencies

--- build-scripts/conanfile.py
+++ build-scripts/conanfile.py
@@ -8,13 +8,15 @@ class conanRecipe(ConanFile):
     settings = ("os", "build_type", "arch")
 
     def configure(self):
-        pass
+        if self.settings.os == "Macos":
+            self.options["libvpx"].shared = True
 
 
     def requirements(self):
         self.requires("videoai/0.5.3")
         if self.settings.os == "Macos":
             self.requires("openh264/2.2.0")
+            self.requires("libvpx/1.11.0")
 
     def imports(self):
         if self.settings.os == "Windows":
--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -8,6 +8,7 @@ class conanRecipe(ConanFile):
 		self.requires("videoai/0.5.3")
 		if self.settings.os == "Macos":
 		    self.requires("openh264/2.2.0")
+		    self.requires("libvpx/1.11.0")
 
 	def imports(self):
 		if self.settings.os == "Windows":
