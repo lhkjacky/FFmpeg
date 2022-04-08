de23985223af55b14bd6ab3a761e31b60ba74d54
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-04-08 13:45:06 -0500

    Use videoai 0.5.4

--- build-scripts/conanfile.py
+++ build-scripts/conanfile.py
@@ -13,7 +13,7 @@ class conanRecipe(ConanFile):
 
 
     def requirements(self):
-        self.requires("videoai/0.5.3")
+        self.requires("videoai/0.5.4")
         if self.settings.os == "Macos":
             self.requires("openh264/2.2.0")
             self.requires("libvpx/1.11.0")
