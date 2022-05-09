e5f99440931ea813c4d229b45303aa3ff501e202
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-05-09 17:28:03 -0500

    bump video ai

--- build-scripts/conanfile.py
+++ build-scripts/conanfile.py
@@ -13,7 +13,7 @@ class conanRecipe(ConanFile):
 
 
     def requirements(self):
-        self.requires("videoai/0.5.9")
+        self.requires("videoai/0.5.10")
         if self.settings.os == "Macos":
             self.requires("openh264/2.2.0")
             self.requires("libvpx/1.11.0")
