72204003df3e238e111bad4f742ae6232b2a207b
Gregory Maddra <gregory.maddra@topazlabs.com>
2022-05-19 14:12:31 -0500

    Remove unneeded zlib dep

--- build-scripts/conanfile.py
+++ build-scripts/conanfile.py
@@ -14,7 +14,6 @@ class conanRecipe(ConanFile):
 
     def requirements(self):
         self.requires("videoai/0.5.13")
-        self.requires("zlib/1.2.12")
         if self.settings.os == "Macos":
             self.requires("openh264/2.2.0")
             self.requires("libvpx/1.11.0")
