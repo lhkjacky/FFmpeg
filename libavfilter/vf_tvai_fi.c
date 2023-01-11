19a628bedd025c2862853f0894e9f87f450606cc
surajTL <suraj.raghuraman@topazlabs.com>
2023-01-11 01:25:45 -0600

    added support for frame interpolation models

--- libavfilter/vf_tvai_fi.c
+++ libavfilter/vf_tvai_fi.c
@@ -43,15 +43,7 @@ typedef struct  {
     double vram;
     int canDownloadModels;
     void* pFrameProcessor;
-    unsigned int count;
-    float fpsFactor;
-    float position;
     AVRational frame_rate;
-    long long previousPts;
-    long long currentPts;
-    int isApollo;
-    int stats;
-    int (*filterFunc)(AVFilterLink *, AVFrame *);
     AVFrame* previousFrame;
 } TVAIFIContext;
 
@@ -77,10 +69,6 @@ int handlePostFlight(void* pProcessor, AVFilterLink *outlink, AVFrame *in, AVFil
 static av_cold int init(AVFilterContext *ctx) {
     TVAIFIContext *tvai = ctx->priv;
     av_log(ctx, AV_LOG_DEBUG, "Init with params: %s %d %d %lf %d/%d = %lf\n", tvai->model, tvai->device, tvai->extraThreads, tvai->slowmo, tvai->frame_rate.num, tvai->frame_rate.den, av_q2d(tvai->frame_rate));
-    tvai->count = 0;
-    tvai->position = 0;
-    tvai->previousPts = 0;
-    tvai->currentPts = 0;
     tvai->previousFrame = NULL;
     return 0;
 }
@@ -90,187 +78,68 @@ static int config_props(AVFilterLink *outlink) {
     TVAIFIContext *tvai = ctx->priv;
     AVFilterLink *inlink = ctx->inputs[0];
     float threshold = 0.05;
+    float fpsFactor = 0;
     if(tvai->frame_rate.num > 0) {
         AVRational frFactor = av_div_q(tvai->frame_rate, inlink->frame_rate);
-        tvai->fpsFactor = 1/(tvai->slowmo*av_q2d(frFactor));
+        fpsFactor = 1/(tvai->slowmo*av_q2d(frFactor));
 
     } else {
         outlink->frame_rate = inlink->frame_rate;
-        tvai->fpsFactor = 1/tvai->slowmo;
+        fpsFactor = 1/tvai->slowmo;
     }
     av_log(ctx, AV_LOG_DEBUG, "Set time base to %d/%d %lf -> %d/%d %lf\n", inlink->time_base.num, inlink->time_base.den, av_q2d(inlink->time_base), outlink->time_base.num, outlink->time_base.den, av_q2d(outlink->time_base));
     av_log(ctx, AV_LOG_DEBUG, "Set frame rate to %lf -> %lf\n", av_q2d(inlink->frame_rate), av_q2d(outlink->frame_rate));
-    av_log(ctx, AV_LOG_DEBUG, "Set fpsFactor to %lf generating %lf frames\n", tvai->fpsFactor, 1/tvai->fpsFactor);
-    threshold = tvai->fpsFactor*0.3;
-    float params[2] = {threshold, 1/tvai->fpsFactor};
-    tvai->isApollo = strncmp(tvai->model, (char*)"apo", 3) == 0;
-    tvai->filterFunc = tvai->isApollo ? filter_frame_apollo : filter_frame_chronos;
-    tvai->pFrameProcessor = ff_tvai_verifyAndCreate(inlink, outlink, tvai->isApollo ? (char*)"apo" : (char*)"chr", tvai->model, ModelTypeFrameInterpolation, tvai->device, tvai->extraThreads, tvai->vram, 1, tvai->canDownloadModels, params, 2, ctx);
+    av_log(ctx, AV_LOG_DEBUG, "Set fpsFactor to %lf generating %lf frames\n", fpsFactor, 1/fpsFactor);
+    threshold = fpsFactor*0.3;
+    float params[2] = {threshold, fpsFactor, tvai->slowmo};
+    tvai->pFrameProcessor = ff_tvai_verifyAndCreate(inlink, outlink, (char*)"fi", tvai->model, ModelTypeFrameInterpolation, tvai->device, tvai->extraThreads, tvai->vram, 1, tvai->canDownloadModels, params, 2, ctx);
     outlink->time_base = inlink->time_base;
     outlink->frame_rate = tvai->frame_rate.num > 0 ? tvai->frame_rate : inlink->frame_rate;
     return tvai->pFrameProcessor == NULL ? AVERROR(EINVAL) : 0;
 }
-
 
 static const enum AVPixelFormat pix_fmts[] = {
     AV_PIX_FMT_RGB48,
     AV_PIX_FMT_NONE
 };
 
-static int filter_frame_chronos(AVFilterLink *inlink, AVFrame *in) {
+static int filter_frame(AVFilterLink *inlink, AVFrame *in) {
     AVFilterContext *ctx = inlink->dst;
     TVAIFIContext *tvai = ctx->priv;
     AVFilterLink *outlink = ctx->outputs[0];
-    AVFrame *out;
-    IOBuffer ioBuffer;
-    float location = 0;
-    static int ocount = 0;
-    ff_tvai_prepareBufferInput(&ioBuffer, in);
-    if(tvai->pFrameProcessor == NULL || tvai_process(tvai->pFrameProcessor,  &ioBuffer, 0)) {
-        av_log(ctx, AV_LOG_ERROR, "The processing has failed adding a frame\n");
+    int ret = 0;
+    if(ff_tvai_process(tvai->pFrameProcessor, in, 0)) {
+        av_log(NULL, AV_LOG_ERROR, "The processing has failed\n");
         av_frame_free(&in);
         return AVERROR(ENOSYS);
-    }
-    while(tvai->position < tvai->count) {
-        out = ff_tvai_prepareBufferOutput(outlink, &ioBuffer.output);
-        location = tvai->position - (tvai->count - 1);
-        av_log(ctx, AV_LOG_DEBUG, "Process frame %f on current %d at %f\n", tvai->position, tvai->count, location);
-        if(tvai->pFrameProcessor == NULL || out == NULL || tvai_interpolator_process(tvai->pFrameProcessor, location, &ioBuffer)) {
-            av_log(ctx, AV_LOG_ERROR, "The processing has failed for intermediate frame\n");
-            av_frame_free(&in);
-            return AVERROR(ENOSYS);
-        }
-        av_frame_copy_props(out, in);
-        out->pts = ((in->pts - tvai->previousPts)*location + in->pts)*tvai->slowmo;
-        ocount++;
-        if(ff_filter_frame(outlink, out)) {
-            av_frame_free(&in);
-            return AVERROR(ENOSYS);
-        }
-        tvai->position += tvai->fpsFactor;
-        av_log(ctx, AV_LOG_DEBUG, "Added frame at pts %lld %lf %d\n", out->pts, av_q2d(inlink->time_base)*out->pts, ocount);
-    }
-    tvai->previousPts = in->pts;
-    av_frame_free(&in);
-    tvai->count++;
-    return 0;
-}
-
-
-static int filter_frame_apollo(AVFilterLink *inlink, AVFrame *in) {
-    AVFilterContext *ctx = inlink->dst;
-    TVAIFIContext *tvai = ctx->priv;
-    AVFilterLink *outlink = ctx->outputs[0];
-    AVFrame *out;
-    IOBuffer ioBuffer;
-    float location = 0;
-    static int ocount = 0;
-    ff_tvai_prepareBufferInput(&ioBuffer, in);
-    if(tvai->pFrameProcessor == NULL || tvai_process(tvai->pFrameProcessor,  &ioBuffer, 0)) {
-        av_log(ctx, AV_LOG_ERROR, "The processing has failed adding a frame\n");
-        av_frame_free(&in);
-        return AVERROR(ENOSYS);
-    }
-    if (tvai->count > 1) {
-        while(tvai->position < tvai->count - 1) {
-            out = ff_tvai_prepareBufferOutput(outlink, &ioBuffer.output);
-            location = tvai->position - (tvai->count - 2);
-            av_log(ctx, AV_LOG_DEBUG, "Process frame %f on current %d at %f\n", tvai->position, tvai->count, location);
-            if(tvai->pFrameProcessor == NULL || out == NULL || tvai_interpolator_process(tvai->pFrameProcessor, location, &ioBuffer)) {
-                av_log(ctx, AV_LOG_ERROR, "The processing has failed for intermediate frame\n");
-                av_frame_free(&in);
-                return AVERROR(ENOSYS);
-            }
-            av_frame_copy_props(out, in);
-            out->pts = ((tvai->currentPts - tvai->previousPts)*location + tvai->previousPts)*tvai->slowmo;
-            ocount++;
-            if(ff_filter_frame(outlink, out)) {
-                av_frame_free(&in);
-                return AVERROR(ENOSYS);
-            }
-            tvai->position += tvai->fpsFactor;
-            av_log(ctx, AV_LOG_DEBUG, "Added frame at pts %lld %lf %d\n", out->pts, av_q2d(inlink->time_base)*out->pts, ocount);
-        }
     }
     if(tvai->previousFrame)
-      av_frame_free(&tvai->previousFrame);
+        av_frame_free(&tvai->previousFrame);
     tvai->previousFrame = in;
-    tvai->previousPts = tvai->currentPts;
-    tvai->currentPts = in->pts;
-    tvai->count++;
-    return 0;
+    ret = ff_tvai_add_output(tvai->pFrameProcessor, outlink, in, 0);
+    return ret;
 }
 
 static int request_frame(AVFilterLink *outlink) {
     AVFilterContext *ctx = outlink->src;
     TVAIFIContext *tvai = ctx->priv;
     int ret = ff_request_frame(ctx->inputs[0]);
-    if (tvai->isApollo && ret == AVERROR_EOF) {
-        if(handlePostFlight(tvai->pFrameProcessor, outlink, tvai->previousFrame, ctx)) {
-          av_log(NULL, AV_LOG_ERROR, "The postflight processing has failed");
-          av_frame_free(&tvai->previousFrame);
-          return AVERROR(ENOSYS);
+    if (ret == AVERROR_EOF) {
+        tvai_end_stream(tvai->pFrameProcessor);
+        while(tvai_remaining_frames(tvai->pFrameProcessor) > 0) {
+            if(ff_tvai_add_output(tvai->pFrameProcessor, outlink, tvai->previousFrame, 0))
+                return ret;
+            tvai_wait(20);
         }
-        av_frame_free(&tvai->previousFrame);
         av_log(ctx, AV_LOG_DEBUG, "End of file reached %s %d\n", tvai->model, tvai->pFrameProcessor == NULL);
     }
     return ret;
 }
 
-int handlePostFlight(void* pProcessor, AVFilterLink *outlink, AVFrame *in, AVFilterContext* ctx) {
-    TVAIFIContext *tvai = ctx->priv;
-    IOBuffer ioBuffer;
-    float location = 0;
-    tvai_end_stream(pProcessor);
-    int i;
-    static int ocount = 0;
-    for(i=0; i<2; i++) {
-        TVAIBuffer oBuffer;
-        AVFrame *out = ff_tvai_prepareBufferOutput(outlink, &oBuffer);
-        if(pProcessor == NULL || out == NULL || tvai_process(pProcessor, &oBuffer, 0)) {
-            av_log(ctx, AV_LOG_ERROR, "The processing has failed");
-            av_frame_free(&in);
-            return AVERROR(ENOSYS);
-        }
-        if (tvai->count > 1) {
-            while(tvai->position < tvai->count - 1) {
-                out = ff_tvai_prepareBufferOutput(outlink, &ioBuffer.output);
-                location = tvai->position - (tvai->count - 2);
-                av_log(ctx, AV_LOG_DEBUG, "Process frame %f on current %d at %f\n", tvai->position, tvai->count, location);
-                if(tvai->pFrameProcessor == NULL || out == NULL || tvai_interpolator_process(tvai->pFrameProcessor, location, &ioBuffer)) {
-                    av_log(ctx, AV_LOG_ERROR, "The processing has failed for intermediate frame\n");
-                    av_frame_free(&in);
-                    return AVERROR(ENOSYS);
-                }
-                av_frame_copy_props(out, in);
-                out->pts = ((tvai->currentPts - tvai->previousPts)*location + tvai->previousPts)*tvai->slowmo;
-                if (out->pts < 0) 
-                    break;
-                ocount++;
-                if(ff_filter_frame(outlink, out)) {
-                    av_frame_free(&in);
-                    return AVERROR(ENOSYS);
-                }
-                tvai->position += tvai->fpsFactor;
-                av_log(ctx, AV_LOG_DEBUG, "Added frame at pts %lld %lf %d\n", out->pts, av_q2d(outlink->time_base)*out->pts, ocount);
-            }
-        }
-        long long ptsDiff = tvai->currentPts - tvai->previousPts;
-        tvai->previousPts = tvai->currentPts;
-        tvai->currentPts += ptsDiff;
-        tvai->count++;
-    }
-    return 0;
-}
-
-static int filter_frame(AVFilterLink *inlink, AVFrame *in) {
-    return ((TVAIFIContext *)inlink->dst->priv)->filterFunc(inlink, in);
-}
-
 static av_cold void uninit(AVFilterContext *ctx) {
     TVAIFIContext *tvai = ctx->priv;
-    if(tvai->pFrameProcessor)
-      tvai_destroy(tvai->pFrameProcessor);
+    // if(tvai->pFrameProcessor)
+    //   tvai_destroy(tvai->pFrameProcessor);
 }
 
 static const AVFilterPad tvai_fi_inputs[] = {
