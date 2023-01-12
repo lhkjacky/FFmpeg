1d325a356e2cb75907ff0fc359959214512a4c4e
Gregory Maddra <gregory.maddra@topazlabs.com>
2023-01-12 16:00:46 -0600

    Update video ai

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -5,7 +5,7 @@ class conanRecipe(ConanFile):
 	settings = ("os", "build_type", "arch")
 
 	def requirements(self):
-		self.requires("videoai/[~0.10.9]")
+		self.requires("videoai/[~0.10.10]")
 		if self.settings.os == "Macos" or self.settings.os == "Linux":
 		    self.requires("libvpx/1.11.0") #libvpx is static on Windows
 		    self.requires("aom/3.5.0")
