6d74e21b140dabf5394c0659945df6eac2295f06
Gregory Maddra <gregory.maddra@topazlabs.com>
2023-01-26 14:35:49 -0600

    Fix tabs

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -1,22 +1,22 @@
 from conans import ConanFile, tools
 
 class conanRecipe(ConanFile):
-	name = "topaz-ffmpeg"
-	settings = ("os", "build_type", "arch")
+    name = "topaz-ffmpeg"
+    settings = ("os", "build_type", "arch")
 
-	def requirements(self):
-		self.requires("videoai/[~0.11.6]")
-		if self.settings.os == "Macos" or self.settings.os == "Linux":
-		    self.requires("libvpx/1.11.0") #libvpx is static on Windows
-		    self.requires("aom/3.5.0")
+    def requirements(self):
+        self.requires("videoai/[~0.11.6]")
+        if self.settings.os == "Macos" or self.settings.os == "Linux":
+            self.requires("libvpx/1.11.0") #libvpx is static on Windows
+            self.requires("aom/3.5.0")
             
     def package_id(self):
         self.info.requires["videoai"].minor_mode()
 
-	def imports(self):
-		if self.settings.os == "Windows":
-			self.copy("*")
-		if self.settings.os == "Macos":
-			self.copy("*")
-		if self.settings.os == "Linux":
-			self.copy("*")
+    def imports(self):
+        if self.settings.os == "Windows":
+            self.copy("*")
+        if self.settings.os == "Macos":
+            self.copy("*")
+        if self.settings.os == "Linux":
+            self.copy("*")
